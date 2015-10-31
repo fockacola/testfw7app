5693197108053be9da43130175eca20553c08153
fockacola <luca@lucas-MacBook-Pro.local>
2015-10-31 17:25:17 +0100

    yfhj

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -51,3 +51,19 @@ function createContentPage() {
     );
 	return;
 }
+var myApp = new Framework7();
+var $$ = Dom7;
+ 
+var mainView = myApp.addView('.view-main');
+ 
+myApp.onPageInit('login-screen', function (page) {
+  var pageContainer = $$(page.container);
+  pageContainer.find('.list-button').on('click', function () {
+    var username = pageContainer.find('input[name="username"]').val();
+    var password = pageContainer.find('input[name="password"]').val();
+    // Handle username and password
+    myApp.alert('Username: ' + username + ', Password: ' + password, function () {
+      mainView.goBack();
+    });
+  });
+});     
